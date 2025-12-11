29493e5c3b3982c66863442c7ad3fc8621745bce
lfabio552 <lfabio552@gmail.com>
2025-12-10 22:28:16 -0300

    Import AppSimplificado in index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './App.css'; // Se você criar este arquivo depois
+import AppSimplificado from './AppSimplificado';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
